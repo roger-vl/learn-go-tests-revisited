d1561918ea3400b9f339c031a46418328830f33e
rogervl <roger23vel@gmail.com>
2025-08-31 20:21:10 -0600

    use benchmarks to measure and improve code

--- three/three.go
+++ three/three.go
@@ -1,16 +1,23 @@
 package three
 
+import "strings"
+
 /* Key ideas:
 *
 * - if we now that a variable is not initializing just use var
 *
+* Benchmark:
+*
+* Use benchmark along the testing
+*
+* check the performance to know if an implementation is actually good
 *
  */
 
 func Repeater(digit string) string {
-	var repeated string
+	var repeated strings.Builder
 	for i := 0; i < 5; i++ {
-		repeated += digit
+		repeated.WriteString(digit)
 	}
-	return repeated
+	return repeated.String()
 }
--- three/three_test.go
+++ three/three_test.go
@@ -8,6 +8,10 @@ import "testing"
 *
 * Requirement: function that repeats a character 5 times
 *
+* New requirement: adding benchmark to improve performance
+*
+* New requirement: improve performance by using string builder
+*
  */
 
 func TestRepeater(t *testing.T) {
@@ -17,4 +21,15 @@ func TestRepeater(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q but want %q", got, want)
 	}
+}
+
+// to actually run this: go test -bench=. ./three/
+
+func BenchmarkRepeated(b *testing.B) {
+	// setup of test
+	for b.Loop() {
+		// running of code to measure
+		Repeater("d")
+	}
+	// cleanup
 }
